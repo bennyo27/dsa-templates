339790bc71fbf09648432d53516faa112d85b94f
bennyo27 <oseguerabenny27@gmail.com>
2024-02-26 19:54:03 -0800

    Fixed: issue

--- src/pages/[tag]/[slug]/index.png.ts
+++ src/pages/[tag]/[slug]/index.png.ts
@@ -9,7 +9,7 @@ export async function getStaticPaths() {
   );
 
   return posts.map(post => ({
-    params: { slug: slugifyStr(post.data.title) },
+    params: { tag: post.data.tags[0], slug: slugifyStr(post.data.title) },
     props: post,
   }));
 }
