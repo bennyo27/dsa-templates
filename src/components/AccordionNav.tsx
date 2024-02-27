66c31e77a6cd2b2a8a5f96f4d9e8510ebfac4689
bennyo27 <oseguerabenny27@gmail.com>
2024-02-26 23:14:14 -0800

    Fixed Navbar

--- src/components/AccordionNav.tsx
+++ src/components/AccordionNav.tsx
@@ -1,17 +1,35 @@
+import React from "react";
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import getSubtopicsByTopic from "@/utils/getSubtopicsByTopic";
+import getUniqueTopics from "@/utils/getUniqueTopics";
+import { getCollection } from "astro:content";
 
-export function AccordionNav() {
+const subtopics = await getCollection("subtopics");
+
+const topics = getUniqueTopics(subtopics);
+
+const AccordionNav = () => {
   return (
     <Accordion type="single" collapsible className="w-full">
-      <AccordionItem value="item-1">
-        <AccordionTrigger>Arrays</AccordionTrigger>
-        <AccordionContent>Kadane's Algorithm</AccordionContent>
-      </AccordionItem>
+      {topics.map(({ topic }, index) => (
+        <AccordionItem key={`${topic + index}`} value="item-1">
+          <AccordionTrigger>
+            {topic.charAt(0).toUpperCase() + topic.slice(1)}
+          </AccordionTrigger>
+          {getSubtopicsByTopic(subtopics, topic).map(subtopic => (
+            <a href={`/${subtopic.data.topics[0]}/${subtopic.slug}`}>
+              <AccordionContent>{subtopic.data.title}</AccordionContent>
+            </a>
+          ))}
+        </AccordionItem>
+      ))}
     </Accordion>
   );
-}
+};
+
+export default AccordionNav;
